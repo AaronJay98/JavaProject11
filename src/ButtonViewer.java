7f5dda6884ecb8f2fd0459d7500cbefd7c4a131e
Aaron Jay Macaldo <aaronjay.macaldo@sjsu.edu>
2019-11-14 20:57:00 -0800

    Finished E3.24 (Olympic Rings) part of assignment

---
+++ src/ButtonViewer.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class ButtonViewer
+{
+        private static final int FRAME_WIDTH = 100;
+        private static final int FRAME_HEIGHT = 60;
+        public static void main(String[] args)
+        {
+        JFrame frame = new JFrame();
+        JButton button1 = new JButton("Button 1");
+        JButton button2 = new JButton("Button 2");
+        frame.add(button1);
+        frame.add(button2);
+        ActionListener listener1 = new ClickListener();
+        ActionListener listener2 = new ClickListener();
+        button1.addActionListener(listener1);
+        button2.addActionListener(listener2);
+
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        }
+}
---
+++ src/OlympicRing.java
@@ -0,0 +1,6 @@
+public class OlympicRing {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
