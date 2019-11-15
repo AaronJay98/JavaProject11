4b29c587d5de9459f4ca644c514ca1fedcfddb50
Aaron Jay Macaldo <Aaronjay.macaldo@sjsu.edu>
2019-11-14 21:27:13 -0800

    Made minor changes to the olympicRing so that its always centered no matter frame size

--- src/OlympicRing.java
+++ src/OlympicRing.java
@@ -1,6 +1,24 @@
+import java.awt.*;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
+
 public class OlympicRing {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static Shape createOlympicRing(
+            double centerX, double centerY, double outerRadius, double thickness)
+    {
+        Ellipse2D outer = new Ellipse2D.Double(
+                centerX - outerRadius,
+                centerY - outerRadius,
+                outerRadius + outerRadius,
+                outerRadius + outerRadius);
+        Ellipse2D inner = new Ellipse2D.Double(
+                centerX - outerRadius + thickness,
+                centerY - outerRadius + thickness,
+                outerRadius + outerRadius - thickness - thickness,
+                outerRadius + outerRadius - thickness - thickness);
+        Area area = new Area(outer);
+        area.subtract(new Area(inner));
+        return area;
     }
 }
--- src/OlympicRingComponent.java
+++ src/OlympicRingComponent.java
@@ -4,38 +4,14 @@ import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
 
 public class OlympicRingComponent extends JComponent {
-    /*
-    public void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g;
-        final int RING_X = getWidth()/5;
-        final int RING_Y = getHeight()/5;
-        final int RING_DIAMETER = 100;
-        Ellipse2D.Double ring = new Ellipse2D.Double(RING_X, RING_Y, RING_DIAMETER, RING_DIAMETER);
-        g2.setColor(Color.blue);
-        g2.drawOval(100, 100, RING_DIAMETER, RING_DIAMETER);
-        g2.fill(ring);
-        g2.setColor(Color.black);
-        g2.translate(RING_DIAMETER, 0);
-        g2.draw(ring);
-        g2.fill(ring);
-        g2.setColor(Color.RED);
-        g2.translate(RING_DIAMETER, 0);
-        g2.draw(ring);
-        g2.fill(ring);
-        g2.setColor(Color.GREEN);
-        g2.translate(-(RING_DIAMETER/2), RING_DIAMETER/2);
-        g2.draw(ring);
-        g2.fill(ring);
-
-    }
-     */
 
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
+        final int RING_THICKNESS = 6;
         final int RING_RADIUS = 50;
-        final int RING_X = getWidth()/5 + RING_RADIUS;
-        final int RING_Y = getHeight()/5 + RING_RADIUS * 2;
-        Shape ring = createRingShape(RING_X, RING_Y, RING_RADIUS, 6);
+        final int RING_X = getWidth()/2 - 2 * RING_RADIUS;
+        final int RING_Y = getHeight()/2 - RING_RADIUS/2 ;
+        Shape ring = OlympicRing.createOlympicRing(RING_X, RING_Y, RING_RADIUS, RING_THICKNESS);
         g2.setColor(Color.BLUE);
         g2.fill(ring);
         g2.draw(ring);
@@ -61,23 +37,7 @@ public class OlympicRingComponent extends JComponent {
         g2.draw(ring);
     }
 
-    private static Shape createRingShape(
-            double centerX, double centerY, double outerRadius, double thickness)
-    {
-        Ellipse2D outer = new Ellipse2D.Double(
-                centerX - outerRadius,
-                centerY - outerRadius,
-                outerRadius + outerRadius,
-                outerRadius + outerRadius);
-        Ellipse2D inner = new Ellipse2D.Double(
-                centerX - outerRadius + thickness,
-                centerY - outerRadius + thickness,
-                outerRadius + outerRadius - thickness - thickness,
-                outerRadius + outerRadius - thickness - thickness);
-        Area area = new Area(outer);
-        area.subtract(new Area(inner));
-        return area;
-    }
+
 
 
 
